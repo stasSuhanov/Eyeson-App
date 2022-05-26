b31418337324bea7eaa2941b8c52188244733b9b
stasnislau <stasikistasik@gmail.com>
2022-05-26 10:59:12 +0300

    Add binding

--- app/src/main/java/com/example/eyesonapp/ui/calls/CallsActivity.kt
+++ app/src/main/java/com/example/eyesonapp/ui/calls/CallsActivity.kt
@@ -3,16 +3,30 @@ package com.example.eyesonapp.ui.calls
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import com.example.eyesonapp.R
+import com.example.eyesonapp.databinding.ActivityCallsBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class CallsActivity : AppCompatActivity() {
 
     private val viewModel by viewModels<CallsViewModel>()
+    private lateinit var binding: ActivityCallsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_calls)
+        binding = ActivityCallsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.endCallButton.setOnClickListener {
+            //TODO
+        }
+
+        binding.muteMicrophoneButton.setOnClickListener {
+            //TODO
+        }
+
+        binding.turnOffCameraButton.setOnClickListener {
+            //TODO
+        }
     }
 }
